1288d27d3098fc98da36a09879bbc04cf238d2f4
nkosyrev <nkosyrev@NKPM>
2026-07-27 00:17:42 +0300

    ch1_comb: p9_func_task_testbench: fix and modify DUT and TB
    Signed-off-by: nkosyrev <nkosyrev@NKPM>

--- ch1_combinational/p9_func_task_testbench/sim/top_tb.v
+++ ch1_combinational/p9_func_task_testbench/sim/top_tb.v
@@ -21,30 +21,66 @@
 
 
 module top_tb();
-    // Inputs
+
+    // DUT inputs
     reg [3:0] a, b;
-    // Outputs
+
+    // DUT outputs
     wire [3:0] sum;
     wire cout;
 
+    // Verif vars
+    reg [4:0] expected_full;
+    reg [3:0] expected_sum;
+    reg expected_cout;
+    integer error_count = 0;
+
     top DUT (a, b, sum, cout);
 
+    // task definition
     task apply_test;
         input [3:0] i_a, i_b;
         begin
             a = i_a; b = i_b;
-            #1; // tasks can wait time
+
+            expected_full = i_a + i_b;
+            expected_sum  = expected_full[3:0];
+            expected_cout = expected_full[4];
+
+            #45; // tasks can wait time
+
+            if ((sum !== expected_sum) || (cout !== expected_cout)) begin
+                $display("ERROR: [%0t] %d + %d -> Received: %d (C:%b) | Expected: %d (C:%b)", 
+                         $time, a, b, sum, cout, expected_sum, expected_cout);
+                error_count = error_count + 1;
+            end else begin
+                $display("OK: [%0t] %d + %d = %d (Carry: %b)", $time, a, b, sum, cout);
+            end
+
+            #5; // to match post-synth/impl timing tests
         end
     endtask
 
     initial begin
+        // FPGA's GSR delay (should be >= 105 ns)
+        #200;
+
         $display("--- Start Simulation ---");
 
-        apply_test(4'd2, 4'd3); // test 1
-        apply_test(4'd10, 4'd5); // test 2
-        apply_test(4'd15, 4'd1); // test 3 (Overflow)
+        apply_test(4'd2, 4'd3);    // test 1 (Usual)
+        apply_test(4'd10, 4'd5);   // test 2 (Max sum)
+        apply_test(4'd15, 4'd1);   // test 3 (Overflow)
+        apply_test(4'd0,  4'd0);   // test 4 (Zeroes)
+        apply_test(4'd15, 4'd15);  // test 5 (Max overflow)
 
         $display("--- End Simulation ---");
+
+        if (error_count == 0) begin
+            $display(">>>> PASSED! <<<<");
+        end else begin
+            $display(">>>> FAILED! Errors found: %0d! <<<<", error_count);
+        end
+
         $finish;
     end
 
--- ch1_combinational/p9_func_task_testbench/sources/top.v
+++ ch1_combinational/p9_func_task_testbench/sources/top.v
@@ -27,22 +27,14 @@ module top(
     output c_out
     );
 
-// funcs and tasks definitions
+    // func definition
     function [4:0] sum4(input [3:0] a, input [3:0] b, input cin);
         begin
             sum4 = a + b + cin;
         end
     endfunction
 
-    task log_operation;
-        input [3:0] a_in, b_in, sum_out;
-        input carry_out;
-        begin
-            $display("LOG: [%0t] %d + %d = %d (Carry: %b)", $time, a_in, b_in, sum_out, carry_out);
-        end
-    endtask
-
-// module's body
+    // module's body
     wire c_in;
     wire [4:0] full_sum;
     assign c_in = 1'b0;
@@ -50,8 +42,5 @@ module top(
     assign full_sum = sum4(in1, in2, c_in);
     assign out = full_sum[3:0];
     assign c_out = full_sum[4];
-
-    always @ (full_sum)
-        log_operation(in1, in2, out, c_out); // log after every change of 'full_sum'
 
 endmodule
